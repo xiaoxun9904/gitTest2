e3a83a9f07f4f2d2d25d9074fa874ec6e24e88bc
xiaoxun9904 <1587476707@qq.com>
2020-06-28 11:14:32 +0800

    iss53最终提交

--- src/gitTest1/World.java
+++ src/gitTest1/World.java
@@ -7,7 +7,8 @@ public class World {
 		System.out.println("hello world");
 System.out.println("master分支上World.java");
 System.out.println("现在是dev分支上进行开发工作");
-
+System.out.println("iss53分支新增代码");
+System.out.println("iss53最终提交");
 
 	}
 
