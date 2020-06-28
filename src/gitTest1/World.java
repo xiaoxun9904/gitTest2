562c7d6bbc245986ff448bd10c19c4d2d1513b7b
xiaoxun9904 <1587476707@qq.com>
2020-06-28 11:08:22 +0800

    hotfix分支紧急提交

--- src/gitTest1/World.java
+++ src/gitTest1/World.java
@@ -5,9 +5,11 @@ public class World {
 	public static void main(String[] args) {
 
 		System.out.println("hello world");
-System.out.println("master分支上World.java");
-System.out.println("现在是dev分支上进行开发工作");
-
+		System.out.println("master分支上World.java");
+		System.out.println("现在是dev分支上进行开发工作");
+		System.out.println("div分支commit");
+		System.out.println("iss53提交代码");
+		System.out.println("hotfix紧急分支代码提交");
 
 	}
 
